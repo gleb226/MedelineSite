bc56b97dd12fcd50e6682ea598dc9e2c3201c975
gmedv <gmedvede2226@gmail.com>
2025-10-21 11:40:26 +0300

    removed all  js because i won`t do counter of visitors!

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const STORAGE_KEY = 'medelin_visits';
-let visits = parseInt(localStorage.getItem(STORAGE_KEY) || '0');
-visits++;
-localStorage.setItem(STORAGE_KEY, visits);
-document.getElementById('visitCount').textContent = visits;
